96b38a805364aa85a8719196a4cc7e8248178a43
Lucas Duarte <lucashdp@outlook.com>
2018-12-15 15:59:08 -0200

    Add home route
    The home can be access by "/" or "/home"

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,9 @@ module.exports = function (app) {
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../..', '/lucas_duarte_esportes.html'));
     });
+    app.get('/home', function (req, res) {
+        res.sendFile(path.join(__dirname, '../..', '/lucas_duarte_esportes.html'));
+    });
     app.get('/login', function (req, res) {
         res.sendFile(path.join(__dirname, '../..', '/form-login.html'));
     });
